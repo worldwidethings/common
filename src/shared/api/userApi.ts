942f851ad7f3e435c4ee57a2e7c44d27a3fd3303
René Gastmeier <rene.gastmeier@coderitter.io>
2025-09-14 19:16:08 +0200

    feat: improve user and user api object interfaces

--- src/shared/api/userApi.ts
+++ src/shared/api/userApi.ts
@@ -1,17 +1,14 @@
 import { Result } from 'coderitter-api-rmc'
 import { User } from '../entity/organization/User'
 
-export interface UserSetPasswordParameters {
-  userId: number
+export interface UserRegisterParameters {
+  email?: string
+  firstName?: string
+  lastName?: string
   password: string
 }
 
-export interface UserLoginParameters {
-  email: string
-  password: string
-}
-
-export class UserLoginResult extends Result {
+export class UserRegisterResult extends Result {
   user: User
 
   constructor(user?: User) {
@@ -20,39 +17,48 @@ export class UserLoginResult extends Result {
   }
 }
 
+export interface UserVerifyMailParameters {
+  registerToken: string
+}
+
+export interface UserLoginParameters {
+  email: string
+  password: string
+}
+
+export class UserLoginResult extends Result {
+  token: string
+  user: User
+
+  constructor(token?: string, user?: User) {
+    super()
+    this.token = token as any
+    this.user = user as any
+  }
+}
 export interface UserCheckTokenParameters {
   token: string
 }
 
 export class UserCheckTokenResult extends Result {
-  valid: boolean
   user: User
 
-  constructor(valid?: boolean, user?: User) {
+  constructor(user?: User) {
     super()
-    this.valid = valid as any
     this.user = user as any
   }
 }
 
-export interface UserRegisterParameters {
-  password: string
-  confirmationPassword: string
-  user: Partial<User>
-}
-
-export interface UserVerifyMailParameters {
-  registerToken: string
-  userId: number
+export interface UserSetPasswordParameters {
+  oldPassword: string
+  newPassword: string
 }
 
 export interface UserSendPasswordResetMailParameters {
-  userId: number
   email: string
 }
 
 export interface UserResetPasswordParameters {
-  userId: number
   passwordResetToken: string
-  password: string
+  newPassword: string
 }
